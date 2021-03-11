d59987396ab1c4792b697bcf48b7fe526c6b7739
Guillaume Pelletier <guillaumep_96@hotmail.fr>
2021-03-11 08:49:51 -0500

    Modification des modeles

--- app/Models/panierTalent.php
+++ app/Models/panierTalent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class panierTalent extends Model
+class PanierTalent extends Model
 {
     use HasFactory;
 }
